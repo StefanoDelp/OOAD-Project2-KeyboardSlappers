5be4d722fc5e905fc59d3553c6f39b5f6e480f85
Stefano <stefanodelp@gmail,com>
2019-09-29 18:55:15 -0600

    Stef Branch.
    Check out the "Stef" folder.  Everything is in the Animals file. Its a notebook not a .py. I did a few of the animals but they will eventually need to be moved into their own files.

---
+++ Stef/Animal.py
@@ -0,0 +1,29 @@
+import abc
+
+
+class Animal:
+    @abc.abstractmethod
+    string makeNoise():
+        pass
+    @abc.abstractmethod
+    string setName(self):
+        pass
+    @abc.abstractmethod
+    string wakeUp(self):
+        pass
+    @abc.abstractmethod
+    int Response(self):
+        pass
+    @abc.abstractmethod
+    string roam(self):
+        pass
+    @abc.abstractmethod
+    bool awake = false:
+        pass
+    @abc.abstractmethod
+    string eat(self):
+        pass
+    @abc.abstractmethod
+    string sleep(self):
+        pass
+
